4021baa1e00d4785f0ebcb34f8d414264c21f068
anosha25 <124650395+anosha25@users.noreply.github.com>
2024-04-04 11:54:19 -0400

    idk

--- JS/script.js
+++ JS/script.js
@@ -32,63 +32,108 @@ $(document).ready(function(){
                 })
 
         })
-        }); k
+        }); 
 
-        
-        $('#triangle-up').click(function(){
+
+        $("#tri").click(function(){
             $(this).css('background-color', 'black');
-            $(this).animate({
-                // 'background-color': 'black',
-                'z-index': 1,
-                top: '200px',
-                left: '250px',
-                width: '700px',
-                height: '1000px',
-                'border-top': '0px',
-                'border-right': '0px',
-                transform: 'rotate(0)'
-          });
+            $(this).css('z-index', '1');
+            $("#e_roon_kang").fadeIn();
+        })
+        
+        
+        });
+
+        $("#exitERoon").click(function(){
+            $("#e_roon_kang").hide();
+            $("#tri").removeClass('clicked');
+            $("#tri").css('background-color', 'transparent');
+
+        })
+
+        // $(document).ready(function(){
+        //     $("#pointyGuy").click(function(){
+        //         $(this).animate({
+        //             'dummy': 1 // Dummy property to trigger animation
+        //         }, {
+        //             step: function(now, fx) {
+        //                 $(this).attr('points', '100,50 1000,50  1000,710  100,710');
+        //             },
+        //             duration: 1000 // Adjust the duration as needed
+        //         });
+        //     });
+        // });
+
+
+
+        $(document).ready(function(){
+            $("#pointyGuy").click(function(){
+                console.log("PointyGuy clicked!");
+                $(this).addClass('clicked');
+                $("#takahashi_kuan").fadeIn();
+                $("#titles").css('z-index', '-1');   
+                $(this).css('z-index', '1');                
+             
+            });
           });
 
-    $(".shape").click(function(){
-        if ($(this).is('circle')) { 
-            $(this).css('display', 'none'); 
-            $("#recCircle").css('display', 'inline');
-        } else if ($(this).is('#rhombus')) {
-            $(this).attr('points', '300,10 900,10 720,490 100,490');
-            var animateElement = document.createElementNS("http://www.w3.org/2000/svg", "animate");
-            animateElement.setAttribute("attributeName", "points");
-            animateElement.setAttribute("dur", "2s");
-            animateElement.setAttribute("repeatCount", "1");
-            animateElement.setAttribute("values", "300,10 900,10 720,490 100,490;" +
-            "100,50 1000,50  1000,710  100,710;");
-            $("#titles").hide();
+          $(document).ready(function(){
+            $("#exitEmi").click(function(){
+                $("#pointyGuy").attr('points', '1270,10 1750,600 1590,900 1250,750');
+                $("#pointyGuy").removeClass('clicked');
+                $("#takahashi_kuan").fadeOut(); // Optionally, hide any associated content
+                $("#titles").css('z-index', '2'); // Reset the z-index of titles if needed
+            });
+        });
 
-            $(this).append(animateElement);
-        } else if ($(this).is('#pointyGuy')) {
-            $(this).attr('points', '1270,10 1750,600 1590,900 1250,750');
-            var animateElement = document.createElementNS("http://www.w3.org/2000/svg", "animate");
-            animateElement.setAttribute("attributeName", "points");
-            animateElement.setAttribute("dur", "2s");
-            animateElement.setAttribute("repeatCount", "1");
-            animateElement.setAttribute("values", "1270,10 1750,600 1590,900 1250,750;" +
-                                                   "100,50 2000,50  2000,910  100,910;");
-            $("#titles").hide();
+
+          $(document).ready(function(){
+            console.log("Document is ready!");
+        });
+        
+
+
+
+
+//     $(".shape").click(function(){
+//         if ($(this).is('circle')) { 
+//             $(this).css('display', 'none'); 
+//             $("#recCircle").css('display', 'inline');
+//         } else if ($(this).is('#rhombus')) {
+//             $(this).attr('points', '300,10 900,10 720,490 100,490');
+//             var animateElement = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+//             animateElement.setAttribute("attributeName", "points");
+//             animateElement.setAttribute("dur", "2s");
+//             animateElement.setAttribute("repeatCount", "1");
+//             animateElement.setAttribute("values", "300,10 900,10 720,490 100,490;" +
+//             "100,50 1000,50  1000,710  100,710;");
+//             $("#titles").hide();
+
+//             $(this).append(animateElement);
+//         } else if ($(this).is('#pointyGuy')) {
+//             $(this).attr('points', '1270,10 1750,600 1590,900 1250,750');
+//             var animateElement = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+//             animateElement.setAttribute("attributeName", "points");
+//             animateElement.setAttribute("dur", "2s");
+//             animateElement.setAttribute("repeatCount", "1");
+//             animateElement.setAttribute("values", "1270,10 1750,600 1590,900 1250,750;" +
+//                                                    "100,50 2000,50  2000,910  100,910;");
+//             $("#titles").hide();
 
                                                    
-            $(this).append(animateElement);
-        } else if ($(this).is('#triangle')) {
-        $(this).attr('points', '120,300 850,800 850,800 230,910');
-        var animateElement = document.createElementNS("http://www.w3.org/2000/svg", "animate");
-        animateElement.setAttribute("attributeName", "points");
-        animateElement.setAttribute("dur", "2s");
-        animateElement.setAttribute("repeatCount", "1");
-        animateElement.setAttribute("values", "120,300 850,800 850,800 230,910;" +
-                                               "100,50 1000,50  1000,710  100,710;");
-                                               $("#titles").hide();
+//             $(this).append(animateElement);
+//         } else if ($(this).is('#triangle')) {
+//         $(this).attr('points', '120,300 850,800 850,800 230,910');
+//         var animateElement = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+//         animateElement.setAttribute("attributeName", "points");
+//         animateElement.setAttribute("dur", "2s");
+//         animateElement.setAttribute("repeatCount", "1");
+//         animateElement.setAttribute("values", "120,300 850,800 850,800 230,910;" +
+//                                                "100,50 1000,50  1000,710  100,710;");
+//                                                $("#titles").hide();
 
-        $(this).append(animateElement);
-    }
-        $(this).css('fill', 'black');     
-    });
-});
+//         $(this).append(animateElement);
+//     }
+//         $(this).css('fill', 'black');     
+//     });
+
